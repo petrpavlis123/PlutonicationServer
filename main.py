d1c8e5ba26ea64a95148a90b39b7682a8306beb9
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-10-27 13:51:09 +0200

    .

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == "__main__":
     app.debug = True
-    socketio.run(app, host="0.0.0.0")
+    socketio.run(app, host="0.0.0.0", port="8090")
--- plutonication/__init__.py
+++ plutonication/__init__.py
@@ -5,6 +5,6 @@ from .events import socketio
 def create_app():
     app = Flask(__name__)
 
-    socketio.init_app(app)
+    socketio.init_app(app, cors_allowed_origins="*")
 
     return app
