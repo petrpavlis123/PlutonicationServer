d95e5c32adac6fa00886f716c42642d33c39bab3
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-11-04 19:44:49 +0100

    breaking changes

--- plutonication/events.py
+++ plutonication/events.py
@@ -1,51 +1,85 @@
 from .extensions import socketio
 from flask_socketio import join_room, emit
 
-@socketio.on('connect')
+@socketio.on("connect")
 def connect():
-    print("Someone connected :D")
-    emit('message', "Someone connected :P", broadcast=True)
+    """
+    Event handler that is fired whenever someone connects.
 
-@socketio.on('create_room')
-def message(data):
+    Useful for debugging.
+    """
+    emit("message", "Someone connected <3", broadcast=True)
+
+@socketio.on("create_room")
+def create_room(data):
+    """
+    Creates a new websocket room.
+
+    Docs: https://socket.io/docs/v3/rooms/
+    """
     room = data["Room"]
-    d = data["Data"]
     join_room(room)
-    print("new room: " + str(room))
-    print('received message: ' + d)
-    emit("message", "Someone connected to room: " + str(room), broadcast=True)
 
-@socketio.on('sign_payload')
+@socketio.on("sign_payload")
 def sign_payload(data):
+    """
+    Event handler used by dApps. Used when requesting signature from wallet.
+
+    You can expect that the wallet will emit either "payload_signature" event or "payload_signature_rejected" event
+    """
     room = data["Room"]
-    print('sign payload: ' + str(data["Data"]))
-    emit("message", data["Data"], room=room)
     emit("sign_payload", data["Data"], room=room)
 
-@socketio.on('sign_raw')
+@socketio.on("sign_raw")
 def sign_raw(data):
+    """
+    Event handler used by dApps. Used when requesting signature from wallet.
+
+    You can expect that the wallet will emit either "raw_signature" event or "raw_signature_rejected" event
+    """
     room = data["Room"]
-    print('sign payload: ' + str(data["Data"]))
-    emit("message", data["Data"], room=room)
     emit("sign_raw", data["Data"], room=room)
 
-@socketio.on('signature')
-def signature(data):
+@socketio.on("payload_signature")
+def payload_signature(data):
+    """
+    Event handler used by wallet, when wallet decides to sign given payload.
+    """
     room = data["Room"]
-    print('signature: ' + str(data["Data"]))
-    emit("message", data["Data"], room=room)
-    emit("signature", data["Data"], room=room)
+    emit("payload_signature", data["Data"], room=room)
 
-@socketio.on('signed_payload')
-def signed_payload(data):
+@socketio.on("payload_signature_rejected")
+def payload_signature_rejected(data):
+    """
+    Event handler used by wallet, when wallet decides to reject the signing of given payload.
+    """
     room = data["Room"]
-    print('signed payload: ' + str(data["Data"]))
-    emit("message", data["Data"], room=room)
-    emit("signed_payload", data["Data"], room=room)
+    emit("payload_signature_rejected", data["Data"], room=room)
 
-@socketio.on('pubkey')
+
+@socketio.on("raw_signature")
+def raw_signature(data):
+    """
+    Event handler used by wallet, when wallet decides to sign given raw message.
+    """
+    room = data["Room"]
+    emit("raw_signature", data["Data"], room=room)
+
+@socketio.on("raw_signature_rejected")
+def raw_signature_rejected(data):
+    """
+    Event handler used by wallet, when wallet decides to reject the signing of given raw message.
+    """
+    room = data["Room"]
+    emit("raw_signature_rejected", data["Data"], room=room)
+
+@socketio.on("pubkey")
 def pubkey(data):
+    """
+    The first event emitted by wallet.
+
+    One of the side-effects is joining a given room.
+    """
     room = data["Room"]
     join_room(room)
-    print('Passing pubkey: ' + str(data["Data"]))
-    emit("receivepubkey", str(data["Data"]), room=room)
+    emit("pubkey", str(data["Data"]), room=room)
