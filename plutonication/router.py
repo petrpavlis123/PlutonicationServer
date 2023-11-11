c066fbfabed8ef0d8f7322e56c8f3ae5eb30d48d
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-11-11 19:22:48 +0100

    Digital ocean deployment guide page

--- plutonication/router.py
+++ plutonication/router.py
@@ -1,10 +1,22 @@
 from flask import render_template
 from .extensions import app
 
+
 @app.route("/")
 def main_page():
     return render_template("main_page.html")
 
+
 @app.route("/docs")
 def docs_page():
     return render_template("docs_page.html")
+
+
+@app.route("/digital_ocean_deploy_guide")
+def deploy_page():
+    return render_template("digital_ocean_deployment_page.html")
+
+
+@app.route("/deploy")
+def deploy_page():
+    return render_template("digital_ocean_deployment_page.html")
