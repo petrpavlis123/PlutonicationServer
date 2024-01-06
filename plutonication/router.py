ae7b2ecb92be212df5a9c80d54b14d9994309015
rostislavlitovkin <rostislavlitovkin@gmail.com>
2024-01-06 21:40:26 +0100

    updated page structure

--- plutonication/router.py
+++ plutonication/router.py
@@ -3,20 +3,31 @@ from .extensions import app
 
 
 @app.route("/")
+@app.route("/docs")
 def main_page():
     return render_template("main_page.html")
 
 
-@app.route("/docs")
-def docs_page():
-    return render_template("docs_page.html")
+@app.route("/docs/server")
+@app.route("/docs/server/flask")
+def flask_server_docs_page():
+    return render_template("flask_server_docs_page.html")
+
+
+@app.route("/docs/javascript")
+def plutonication_javascript_page():
+    return render_template("plutonication_javascript_page.html")
+
+
+@app.route("/docs/csharp")
+def plutonication_csharp_page():
+    return render_template("plutonication_csharp_page.html")
 
 
 @app.route("/digital_ocean_deploy_guide")
-def digital_ocean_deploy_page():
-    return render_template("digital_ocean_deployment_page.html")
-
-
 @app.route("/deploy")
-def deploy_page():
+@app.route("/deploy/flask")
+@app.route("/deploy/flask-digital-ocean")
+def digital_ocean_deployment_page():
     return render_template("digital_ocean_deployment_page.html")
+
