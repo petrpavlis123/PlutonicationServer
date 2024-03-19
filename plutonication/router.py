41ae254f2ce0df02d21a1f6cc091d30493c3f92e
rostislavlitovkin <rostislavlitovkin@gmail.com>
2024-03-19 12:11:12 +0100

    udpated version format

--- plutonication/router.py
+++ plutonication/router.py
@@ -41,7 +41,8 @@ def digital_ocean_deployment_page():
 def get_plutowallet_latest_version():
     return {
         "message": "",
-        "version": "1.9",
+        "versionString": "1.10",
+        "version": 11,
     }
 
 # Galaxy Logic Game part
