9af0767465bf2959353a6189005403d1f4001563
rostislavlitovkin <rostislavlitovkin@gmail.com>
2024-04-23 11:43:09 +0200

    hydradx-icon  png

--- plutonication/dapp_icons.py
+++ plutonication/dapp_icons.py
@@ -4,5 +4,5 @@ from .extensions import app
 
 @app.route("/dapp/hydradx-icon")
 def hydradx_icon():
-    return send_file("static/dapp_icons/hydradx-icon.svg", mimetype='image/svg')
+    return send_file("static/dapp_icons/hydradx-icon.png", mimetype='image/png')
 
